a43b8a26bcb14277bf22e5c08d767bf32ca55334
Sridhar Sarnobat <ss401533@gmail.com>
2025-11-06 23:10:52 -0500

    2025-11-06: Makefile iterm_4panes.sh receiver.cpp

--- 11_simulator_redis_pubsub/iterm_4panes.sh
+++ 11_simulator_redis_pubsub/iterm_4panes.sh
@@ -8,25 +8,25 @@ tell application "iTerm"
         -- pane 1 (top-left)
         set pane1 to current session of myTab
         tell pane1
-            write text "cd /Volumes/git/github/depot_parent/cpp/11_simulator && make run-server-ingest"
+            write text "cd $PWD && make run-server-ingest"
         end tell
 
         -- split vertically -> right column (top-right)
         set pane2 to split vertically with default profile of pane1
         tell pane2
-            write text "cd /Volumes/git/github/depot_parent/cpp/11_simulator && make run-server-forward"
+            write text "cd $PWD && make run-server-forward"
         end tell
 
         -- split pane1 horizontally -> bottom-left
         set pane3 to split horizontally with default profile of pane1
         tell pane3
-            write text "cd /Volumes/git/github/depot_parent/cpp/11_simulator && make run-receiver"
+            write text "cd $PWD && make run-receiver"
         end tell
 
         -- split pane2 horizontally -> bottom-right
         set pane4 to split horizontally with default profile of pane2
         tell pane4
-            write text "cd /Volumes/git/github/depot_parent/cpp/11_simulator && make run-clients"
+            write text "cd $PWD && make run-clients"
         end tell
     end tell
 end tell
--- 11_simulator_redis_pubsub/src/receiver.cpp
+++ 11_simulator_redis_pubsub/src/receiver.cpp
@@ -10,23 +10,45 @@
 #include <mutex>
 
 int main() {
-    cpp_redis::subscriber sub;
-    sub.connect();
+    cpp_redis::subscriber redis_subscriber;
+    redis_subscriber.connect();
 
+    // known[0] -> client0's latest message
+    // known[1] -> client1's latest message
+    // known[2] -> client2's latest message
+    // known[3] -> client3's latest message
+    // known[4] -> client4's latest message
     std::vector<std::string> known(5, "");
     std::mutex mtx;
 
-    sub.subscribe("locations_out",
+    redis_subscriber.subscribe("locations_out",
+        //
+        // --- START OF CALLBACK FUNCTION ---
+        //
+        // This is a lambda function that acts as the callback.
+        // It captures variables by reference [&] so it can access `mtx` and `known`.
         [&](const std::string& chan, const std::string& msg) {
+
+            //
+            // --- Parse the incoming message ---
+            //
+
             // parse "id,lat,lon"
             auto pos = msg.find(',');
             if (pos == std::string::npos) return;
             int id = std::stoi(msg.substr(0, pos));
+
+            //
+            // --- Update shared state safely ---
+            //
             {
                 std::lock_guard<std::mutex> lk(mtx);
                 if (id >= 0 && id < 5) known[id] = msg;
             }
 
+            //
+            // --- Print the latest known positions ---
+            //
             std::cout << "\n[receiver] latest positions:\n";
             for (int i = 0; i < 5; ++i) {
                 std::lock_guard<std::mutex> lk(mtx);
@@ -35,7 +57,8 @@ int main() {
             }
         });
 
-    sub.commit();
+    redis_subscriber.commit();
+    // Shut down if we are idle for 60 seconds
     while (true) std::this_thread::sleep_for(std::chrono::seconds(60));
     return 0;
 }
